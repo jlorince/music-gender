5375197d036a363d882e8c6c872f0e5d7d441394
Jared <jared.j.lorince@gmail.com>
2016-09-08 11:11:49 -0500

    arg fix

--- exploratory_analyses.py
+++ exploratory_analyses.py
@@ -26,7 +26,7 @@ class gender_stuff(object):
         self.df = pd.read_table(self.args.file,header=None,names=['artist_id','ts'])
 
         self.filename = self.args.file.split('/')[-1]
-        self.user = filename[:filename.find('.')]
+        self.user = self.filename[:self.filename.find('.')]
 
 
     def rolling_new_artist_mean(self,window_size=100):
