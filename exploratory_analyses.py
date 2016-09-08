e7b641e21d6132b957ca2cde97884cc98924b588
Jared <jared.j.lorince@gmail.com>
2016-09-08 11:06:44 -0500

    arg fix

--- exploratory_analyses.py
+++ exploratory_analyses.py
@@ -57,9 +57,9 @@ if __name__ == '__main__':
     parser.add_argument("-f", "--file",default=None,type=str)
     parser.add_argument("-r", "--resultdir",default=None,type=str)
 
-    args = parser.parse_args(args)
+    args = parser.parse_args()
 
-    g = gender_stuff()
+    g = gender_stuff(args)
     g.rolling_new_artist_mean()
 
 
