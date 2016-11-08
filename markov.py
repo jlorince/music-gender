4ca85460f11cd71e16a57e715eab03185fefb17a
Jared <jared.j.lorince@gmail.com>
2016-11-08 11:00:03 -0600

    minor fix

--- markov.py
+++ markov.py
@@ -9,27 +9,29 @@ import signal
 import sys
 import math
 
-def save_sparse_csr(filename,array):
-    np.savez(filename,data = array.data ,indices=array.indices,
-             indptr =array.indptr, shape=array.shape )
+if __name__ != '__main__':
+    
+    def save_sparse_csr(filename,array):
+        np.savez(filename,data = array.data ,indices=array.indices,
+                 indptr =array.indptr, shape=array.shape )
 
-def load_sparse_csr(filename):
-    loader = np.load(filename)
-    return csr_matrix((  loader['data'], loader['indices'], loader['indptr']),
-                         shape = loader['shape'])
-
-
-artist_map = pd.read_pickle('P:/Projects/BigMusic/jared.data/artist-map-w2v-200-15.pkl').sort_values('idx')[:10000]
-mapping = dict(zip(artist_map['id'],artist_map['idx']))
+    def load_sparse_csr(filename):
+        loader = np.load(filename)
+        return csr_matrix((  loader['data'], loader['indices'], loader['indptr']),
+                             shape = loader['shape'])
 
 
-def parse_df(fi,include_time=False):
-    return pd.read_table(fi,header=None,names=['song_id','artist_id','ts'],usecols=['artist_id'])
+    artist_map = pd.read_pickle('P:/Projects/BigMusic/jared.data/artist-map-w2v-200-15.pkl').sort_values('idx')[:10000]
+    mapping = dict(zip(artist_map['id'],artist_map['idx']))
+
+
+    def parse_df(fi,include_time=False):
+        return pd.read_table(fi,header=None,names=['song_id','artist_id','ts'],usecols=['artist_id'])
 
 
 def process(fi):
     df = parse_df(fi)
-    df['idx'] = df['artist'].apply(lambda x: mapping.get(x,10000))
+    df['idx'] = df['artist_id'].apply(lambda x: mapping.get(x,10000))
     df['last'] = df['idx'].shift(1)
     df.set_value(0,'last',10000)
     df['transition'] = df.apply(lambda row: (int(row['last']),row['idx']) ,axis=1)
